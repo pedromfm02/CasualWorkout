7b4971ca287f7114995382af3220e9a86d2fe7a5
Pedro Matos <pedromfm02@ua.pt>
2022-05-17 09:26:35 +0100

    Initial commit

---
+++ app/src/main/java/com/example/casualworkout/Criar_Treinos.java
@@ -0,0 +1,14 @@
+package com.example.casualworkout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Criar_Treinos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_criar_treinos);
+    }
+}
---
+++ app/src/main/java/com/example/casualworkout/Treino.java
@@ -0,0 +1,14 @@
+package com.example.casualworkout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Treino extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_treino);
+    }
+}
