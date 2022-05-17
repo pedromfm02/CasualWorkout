7a2e8a391ee7521af5dea0bfc6d3a3ba0491817a
Pedro Matos <pedromfm02@ua.pt>
2022-05-17 10:50:41 +0100

    pedro 17/05 1

--- app/src/main/java/com/example/casualworkout/Criar_Treinos.java
+++ app/src/main/java/com/example/casualworkout/Criar_Treinos.java
@@ -2,13 +2,34 @@ package com.example.casualworkout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class Criar_Treinos extends AppCompatActivity {
+
+    private static final String LOG_TAG = Criar_Treinos.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_criar_treinos);
+    }
+
+    public void Save(View view) {
+        Log.d(LOG_TAG, "Save");
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void Cancel(View view) {
+        Log.d(LOG_TAG, "Cancel");
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void AddEx(View view) {
+        Log.d(LOG_TAG, "Add Exercise");
     }
 }
---
+++ app/src/main/java/com/example/casualworkout/Exercicios.java
@@ -0,0 +1,17 @@
+package com.example.casualworkout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class Exercicios extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercicios);
+    }
+
+
+}
