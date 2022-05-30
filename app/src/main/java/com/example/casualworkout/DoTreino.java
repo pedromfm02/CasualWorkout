fc1556990479912de19a1cda66902c66bcc7a443
Afonso Azevedo <afonso.azevedo@ua.pt>
2022-05-30 22:32:21 +0100

    30/5/2022 Afonso 2

--- app/src/main/java/com/example/casualworkout/DoTreino.java
+++ app/src/main/java/com/example/casualworkout/DoTreino.java
@@ -1,14 +1,26 @@
 package com.example.casualworkout;
 
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Chronometer;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+;
+
 
 public class DoTreino extends AppCompatActivity {
+
+    Chronometer chronometer;
+    Button btReset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_do_treino);
+
+        chronometer = findViewById(R.id.chronometer);
+        btReset = findViewById(R.id.bt_reset);
+
     }
 }
